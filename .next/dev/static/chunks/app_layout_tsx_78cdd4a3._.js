bbfb9e024a8124ccb0c85411f009e808d2b4cf26
Chattie Developer <dev@chattie.app>
2026-01-16 22:49:09 +0530

    chore: remove .next build folder from git

--- .next/dev/static/chunks/app_layout_tsx_78cdd4a3._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__59cd3dcd._.js",
-  "static/chunks/[root-of-the-server]__9f48fe3c._.css"
-],
-    source: "dynamic"
-});
